73ce8d4b571f55449d61e6ec3b253a9065f6f0a7
Edgar Arturo Martínez <edgar.arturo.martinez@gmail.com>
2022-09-15 12:34:15 -0500

    Application WinForm CRUD with Entity Framework

---
+++ WinForms_EF_CRUD/WinForms_EF_CRUD/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinForms_EF_CRUD.Models;
+
+namespace WinForms_EF_CRUD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        #region HELPER
+        private void Refresh()
+        {
+            using (TestDBEntities db = new TestDBEntities())
+            {
+                var lst = from d in db.UserInfoes
+                          select d;
+                dataGridView1.DataSource = lst.ToList();
+            }
+        }
+
+        private int? GetId()
+        {
+            try
+            {
+                return int.Parse(dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[0].Value.ToString());
+            }
+            catch 
+            {
+
+                return null;
+            }
+            
+        }
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Presentation.FrmUserInfo objUserInfo = new Presentation.FrmUserInfo();
+            objUserInfo.ShowDialog(); //Abrir cuadro de dialogo Hijo
+
+            Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int? id = GetId();
+            if (id != null)
+            {
+                Presentation.FrmUserInfo objUserInfo = new Presentation.FrmUserInfo(id);
+                objUserInfo.ShowDialog(); //abrir cuadro de dialogo Hijo
+
+                Refresh();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int? id = GetId();
+            if (id != null)
+            {
+                using (TestDBEntities db = new TestDBEntities())
+                {
+                    UserInfo objUserInfo = db.UserInfoes.Find(id);
+                    db.UserInfoes.Remove(objUserInfo);
+
+                    db.SaveChanges();
+                }
+
+                Refresh();
+            }
+        }
+    }
+}
---
+++ WinForms_EF_CRUD/WinForms_EF_CRUD/Presentation/FrmUserInfo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinForms_EF_CRUD.Models;
+
+namespace WinForms_EF_CRUD.Presentation
+{
+    public partial class FrmUserInfo : Form
+    {
+        public int? id;
+        UserInfo objUserInfo = null;
+        public FrmUserInfo(int? id=null)
+        {
+            InitializeComponent();
+            this.id = id;
+            if (id != null)
+            {
+                LoadData();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (TestDBEntities db = new TestDBEntities())
+            {
+                if (id == null)
+                {
+                    objUserInfo = new UserInfo();
+                }
+                 
+                objUserInfo.Nombre = txtNombre.Text;
+                objUserInfo.Correo = txtCorreo.Text;
+                objUserInfo.Fecha_Nacimiento = dtFechaNacimiento.Value;
+
+                if (id == null)
+                {
+                    db.UserInfoes.Add(objUserInfo); //Guardar en entity
+                }
+                else
+                {
+                    db.Entry(objUserInfo).State = System.Data.Entity.EntityState.Modified;
+                }
+                
+                db.SaveChanges(); //Guardar en la base de datos
+
+                this.Close();
+            }            
+        }
+
+        private void LoadData()
+        {
+            using (TestDBEntities db = new TestDBEntities())
+            {
+                objUserInfo = db.UserInfoes.Find(id);
+                txtNombre.Text = objUserInfo.Nombre;
+                txtCorreo.Text = objUserInfo.Correo;
+                dtFechaNacimiento.Value = objUserInfo.Fecha_Nacimiento;
+            }
+        }
+    }
+}
